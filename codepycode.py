1ba962adef746ef7ce814bedea654e923d83c212
Zineb49 <lahrizizineb1@gmail.com>
2024-01-13 06:47:30 -0800

    test py code style task 1

--- codepycode.py
+++ codepycode.py
@@ -1,17 +1,4 @@
-def calculate_square_area(side_length):
-    """
-    Calculate the area of a square.
-    Parameters:
-    - side_length (float or int): The length of one side of the square.
-    Returns:
-    float or int: The area of the square.
-    """
-    area = side_length ** 2
-    return area
-
-
-if __name__ == "__main__":
-    # Example usage
-    side_length = 5
-    square_area = calculate_square_area(side_length)
-    print(f"The area of a square {side_length} is: {square_area}")
+#!/usr/bin/python3
+def add(a, b):
+    sum = a + b
+    return sum
